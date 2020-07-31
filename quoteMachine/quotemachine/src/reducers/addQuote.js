62bd407b9e45499164a3cded7b9c2dd57d4204ec
Maung Naing <mpnaing@ucdavis.edu>
2020-07-30 20:07:34 -0700

    Ability to add multiple quotes - just both of them are in an array

--- quoteMachine/quotemachine/src/reducers/addQuote.js
+++ quoteMachine/quotemachine/src/reducers/addQuote.js
@@ -1,9 +1,14 @@
-const addQuoteReducer = (state={}, action) => {
+const initialState = {
+    quotes: [],
+    authors: []
+}
+
+const addQuoteReducer = (state=initialState, action) => {
     switch(action.type){
         case 'ADD_QUOTE':
             return (Object.assign({}, state, {
-                author: action.author,
-                quote: action.quote
+                authors: state.authors.concat(action.author),
+                quotes: state.quotes.concat(action.quote)
             }));
         default:
             return state;
