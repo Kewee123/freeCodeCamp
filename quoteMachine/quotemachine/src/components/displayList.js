94d7f6e0f1389a42dab1401ca3513e250e1922ce
Maung Naing <mpnaing@ucdavis.edu>
2020-07-31 19:54:21 -0700

    quoteMachine: adding id's and trying to figure out how to add api call to redux

--- quoteMachine/quotemachine/src/components/displayList.js
+++ quoteMachine/quotemachine/src/components/displayList.js
@@ -10,7 +10,10 @@ const mapStateToProps = state => {
 const DisplayList = ({ quotes }) => (
     <div>
         {quotes.map(item => {
-            return <li key={item.author}>{item.author}: "{item.quote}"</li>
+            return <div id="quote-box" key={item.author}>
+                <div id="author">{item.author}</div> 
+                <div id="text">"{item.quote}"</div>
+            </div>
         })}
     </div>
 );
