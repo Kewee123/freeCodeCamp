104408ba6f468708117ede3841102058645aaa30
Maung Naing <mpnaing@ucdavis.edu>
2020-08-02 20:22:34 -0700

    quoteMachine: randomly retrieve a quote from the list of 100

--- quoteMachine/quotemachine/src/components/displayQuote.js
+++ quoteMachine/quotemachine/src/components/displayQuote.js
@@ -10,29 +10,42 @@ const mapStateToProps = state => {
     };
 };
 
-const mapDispatchToProps = dispatch => {
-    return{
-        random_quote: () => dispatch(random_quote())
-    }
-}
-
-
 class DisplayQuote extends Component {
     constructor(props){
         super(props);
 
         this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            retrieveQuote : "",
+            fetched: false
+        }
+    }
+
+    componentDidUpdate(prevProps,prevState){
+        console.log("yo bro", this.props.quote);
+        if( prevProps && (this.props.fetched !== this.state.fetched)){
+            this.setState ({
+                retrieveQuote: this.props.quote[Math.floor(Math.random() * this.props.quote.length)].starWarsQuote,
+                fetched: this.props.fetched
+            })
+        }
+
+        console.log("component did mount", this.state)
     }
 
     handleSubmit(event){
-        this.props.random_quote()
+        this.setState ({
+            retrieveQuote: this.props.quote[Math.floor(Math.random() * this.props.quote.length)].starWarsQuote
+        })
     }
 
     render() {
         const {quote, fetched} = this.props;
+        console.log("displayQuote state"+ this.state.retrieveQuote);
         return (
             <div>
-                {fetched && quote[0].starWarsQuote}
+                {fetched && this.state.retrieveQuote}
                 <br/>
                 <button id="random_quote" onClick={this.handleSubmit}>Random Quote</button>
              </div>
@@ -42,4 +55,4 @@ class DisplayQuote extends Component {
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(DisplayQuote);
+export default connect(mapStateToProps)(DisplayQuote);
